ac4bc92a6eefee30f0ed22cf50efee90cdc813e9
shakib04 <51458806+shakib04@users.noreply.github.com>
2021-07-01 03:05:24 +0600

    project init and basic start

---
+++ Codebase_Books_Cabinet/Books_Cabinet/src/main/java/com/project/Books_Cabinet/BooksCabinetApplication.java
@@ -0,0 +1,13 @@
+package com.project.Books_Cabinet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksCabinetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksCabinetApplication.class, args);
+	}
+
+}
---
+++ Codebase_Books_Cabinet/Books_Cabinet/src/main/java/com/project/Books_Cabinet/controller/HomeController.java
@@ -0,0 +1,30 @@
+package com.project.Books_Cabinet.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/home")
+	private String homepage() {
+
+		return "homepage.html";
+	}
+
+	@PostMapping("/contactForm")
+	private ModelAndView contactForm(String fullname,String message, String title, Model model) {
+		//model.addAttribute("fullname", fullname);
+		//System.out.println(fullname);
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("contact.html");
+		
+		mv.addObject("fullname", fullname);
+		//mv.addObject("title", title);
+		
+		return mv;
+	}
+}
