30573c979ba06facd7022dc4d4f6584b60f0211e
fesabelilla <fesabelilla@gmail.com>
2021-07-04 21:59:17 +0600

    HomeDesign

--- Codebase_Books_Cabinet/Books_Cabinet/src/main/java/com/project/Books_Cabinet/Controller/HomeController.java
+++ Codebase_Books_Cabinet/Books_Cabinet/src/main/java/com/project/Books_Cabinet/Controller/HomeController.java
@@ -1,4 +1,4 @@
-package com.project.Books_Cabinet.controller;
+package com.project.Books_Cabinet.Controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
